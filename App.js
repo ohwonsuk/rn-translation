dea7a76bcbed0baedfe5a76d04b67ff7cc0e0107
ohwonsuk <wsoh51@gmail.com>
2024-09-25 20:58:36 +0900

    지역언어 설정 적용 테스트

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getLocales } from 'expo-localization';
+import { I18n } from 'i18n-js';
+
+// Set the key-value pairs for the different languages you want to support.
+const i18n = new I18n({
+  en: { welcome: 'Hello' },
+  ja: { welcome: 'こんにちは' },
+});
+
+// Set the locale once at the beginning of your app.
+const deviceLanguage = getLocales()[0].languageCode;
+i18n.locale = deviceLanguage;
+
+console.log(i18n.t('welcome'));
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{deviceLanguage}</Text>
+      <Text>{i18n.t('welcome')}</Text>
       <StatusBar style="auto" />
     </View>
   );
